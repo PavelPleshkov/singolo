4b1dfe6e5a302071f66c79cd95bb929dd360673c
Pavel Pleshkov <pleshkov.p@gmail.com>
2020-03-14 19:08:55 +0300

    feat: add close button to popup, style for popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
 const mainMenu = document.querySelector('#mainMenu');
+const messageBlock = document.querySelector('#form-message-block');
+const btnSend = document.querySelector('#btnSend');
+const btnCloseMessageBlock = document.querySelector('#message-btn-close');
 
 mainMenu.addEventListener('click', (event) => {
   mainMenu.querySelectorAll('.header-navigation-item').forEach(elem => elem.classList.remove('active'));
   event.target.classList.add('active');
 });
+
+btnSend.addEventListener('click', () => messageBlock.classList.remove('hidden'));
+
+btnCloseMessageBlock.addEventListener('click', () => messageBlock.classList.add('hidden'));
