a4166f80519d15af4dc47082af6a3415ee52d9f1
Pavel Pleshkov <pleshkov.p@gmail.com>
2020-03-24 16:21:08 +0300

    feat: add scrolling to sections by pressing menu items

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,47 @@
+//add scrolling to sections by pressing menu items
 const mainMenu = document.querySelector('#mainMenu');
 const mainSections = [];
+
 document.querySelectorAll('#main>section').forEach((section) => {
   if (section.getAttribute('id') != null) {
     mainSections.push(section);
   }
 });
-// console.log(mainSections);
-document.querySelector('.header-navigation-item:first-child').addEventListener('click', () => {
-  // window.scrollTo(0, 96);
-  // document.scrollY(0);
-  // window.scrollY = '96px';
-  // console.log(document.querySelector('#slider').offsetTop);
-})
-
-
-const messageBlock = document.querySelector('#form-message-block');
-const btnSend = document.querySelector('#btnSend');
-const btnCloseMessageBlock = document.querySelector('#message-btn-close');
-
 document.addEventListener('scroll', onScroll);
 
 function onScroll(event) {
   // console.log(event);
-  const curPos = window.scrollY;
+  const curPos = window.scrollY + document.querySelector('#header').offsetHeight + 1;
 
-  // console.log(curPos);
-  // console.log(document.querySelectorAll('#mainMenu>a'));
-  // document.querySelectorAll('#main>section')
   mainSections.forEach((elem) => {
-    // if (elem.getAttribute('id') != null) {
-      // pageItems.push(elem);
-      // console.log(elem);
-      // console.log(elem.offsetTop);
-      console.log(elem.getAttribute('id'));
-      // console.log(pageItems);
-      if (elem.offsetTop <= curPos && (elem.offsetTop + elem.offsetHight) > curPos) {
-        mainMenu.querySelectorAll('.header-navigation-item').forEach(a => a.classList.remove('active'));
-        if (elem.getAttribute('id') === a.getAttribute('href').substring(1)) {
-          a.classList.add('active');
-        }
-      }
-    // }
-    
-    
-    // elem.getAttribute('id');
+    // console.log(elem.getAttribute('id') + ':\n', 'top of section ' + elem.offsetTop + '\n', 'height of section '+ elem.offsetHeight + ' = ', elem.offsetTop + elem.offsetHeight + '');
+    // console.log(pageYOffset, elem.offsetTop);
+    // console.log(elem.offsetTop);
+    if (elem.offsetTop <= curPos && (elem.offsetTop + elem.offsetHeight) > curPos) {
+      mainMenu.querySelectorAll('.header-navigation-item').forEach(a => {a.classList.remove('active')
+      if (elem.getAttribute('id') === a.getAttribute('href').substring(1)) {
+        a.classList.add('active');
+        // console.log(pageYOffset, elem.offsetTop);
+      }});
+    }
   });
-  
 }
 
 mainMenu.addEventListener('click', (event) => {
-  mainMenu.querySelectorAll('.header-navigation-item').forEach(elem => elem.classList.remove('active'));
+  mainMenu.querySelectorAll('.header-navigation-item').forEach(elem => {
+    elem.classList.remove('active');
+  });
   event.target.classList.add('active');
+  event.preventDefault();
+  window.scrollTo(0, document.getElementById(`${event.target.getAttribute('href').substring(1)}`).offsetTop - document.querySelector('#header').offsetHeight);
 });
 
-btnSend.addEventListener('click', () => messageBlock.classList.remove('hidden'));
 
+
+//add popup to form
+const messageBlock = document.querySelector('#form-message-block');
+const btnSend = document.querySelector('#btnSend');
+const btnCloseMessageBlock = document.querySelector('#message-btn-close');
+
+btnSend.addEventListener('click', () => messageBlock.classList.remove('hidden'));
 btnCloseMessageBlock.addEventListener('click', () => messageBlock.classList.add('hidden'));
